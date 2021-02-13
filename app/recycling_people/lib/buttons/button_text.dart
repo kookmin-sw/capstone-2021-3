eaa9e5754cd50ad0b63446f82cf548e07c77b0b4
TongSeola <waitflowerofeast@gmail.com>
2021-02-13 21:18:03 +0900

    add recyle type image in button

--- app/recycling_people/lib/buttons/button_text.dart
+++ app/recycling_people/lib/buttons/button_text.dart
@@ -1,67 +1,83 @@
 import 'package:flutter/material.dart';
 
-// 분류 모델 11가지
-// 종이류, 금속캔, 고철류, 유리병류, 플라스틱류, 비닐류, 스티로폼, 음식물, 재사용, 종이팩, 투명패트
+// 분류 모델 8가지
+// 캔류, 종이, 플라스틱, 유리, 페트, 비닐류, 종이팩, 스티로폼
 
-//StatefulWidget
-class ButtonText extends StatelessWidget {
-  int type_index;    //원하는 분리배출표시 리스트의 순서
+// StatefulWidget
+// StatelessWidget
+class ButtonText extends StatefulWidget{
+  int type_index; //버튼 작동 여부 확인을 위한 임의 값
+  String type;
+  ButtonText(this.type_index);
+
+  _ButtonText createState() => _ButtonText(this.type_index);
+}
+
+class _ButtonText extends State<ButtonText> {
+  String type;    //원하는 분리배출표시
   String blanck = '          ';
 
-  // 분리배출표시 도안 종류 (7개)
-  final List<String> _type = [
-    '페트',
-    '플라스틱', 
-    '비닐류',
-    '캔류',
-    '종이팩',
-    '종이',
-    '유리'
-  ];
+  // 분리배출표시 도안 종류(8개) + 투명색
+  final Map<String, Color> type_map = {
+    '캔류' : Colors.grey,
+    '종이' : Colors.black,
+    '플라스틱' : Colors.blue,
+    '유리' : Colors.orange,
+    '페트' : Colors.yellow,
+    '비닐류' : Colors.purple,
+    '종이팩' : Colors.green,
+    '스티로폼' : Colors.pink,
+    'None' : Colors.transparent
+  };
 
-  // 각 분리배출표시 도안의 색상
-  final List<Color> _color = [
-    Colors.yellow,
-    Colors.blue,
-    Colors.purple,
-    Colors.grey,
-    Colors.green,
-    Colors.black,
-    Colors.orange
-  ];
+
+  // 버튼 작동 여부 확인을 위한 임의 변수
+  int type_index = 0; 
+  final List<String> type_list = [
+    '캔류', '종이', '플라스틱', '유리', '페트', '비닐류', '종이팩', '스티로폼', 'None'
+  ]; 
+
+
+
+  // type 값을 가져온다. 
+  _ButtonText(this.type_index);
 
   void _onPressed(){
-    type_index ++;
-    if(type_index == 7) {type_index = 0;}
-  }
+    setState(() {
+      type_index ++;
+      if(type_index == 9) {type_index = 0;}
 
-  // 처음에 index를 받아와야 한다
-  ButtonText(this.type_index);
+      type = type_list[type_index];
+    });    
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: _onPressed, 
+      return TextButton(
+      onPressed: _onPressed,
       child: Container(
-        // margin: EdgeInsets.all(10),
-        color: _color[type_index],
         height: 30,
         width: 150,
+        decoration: new BoxDecoration(
+          color: type_map[type_list[type_index]],
+          border: new Border.all(color: type_map[type_list[type_index]], width: 2.0),
+          borderRadius: new BorderRadius.circular(5.0),
+          ),
         child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Icon(Icons.play_arrow),
+                Image.asset('assets/recyleTypeImage/${type_list[type_index]}.jpg'),
                 Text(
-                  _type[type_index],
+                  type_list[type_index],
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white
                   )
                 ),
-                Text(this.blanck.substring(0, 2*_type[type_index].length)) // 보일려는 글자의 2배 크기의 space 만들기
+                Text(this.blanck.substring(0, 2*type_list[type_index].length)) // 보일려는 글자의 2배 크기의 space 만들기
               ]
             )
-      )
+      ) 
     );
   }
 }
