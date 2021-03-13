cd37669099fe5a1d56649d3a9da11504ad8aeb7e
TongSeola <waitflowerofeast@gmail.com>
2021-03-13 12:07:39 +0900

    make navigation bar

---
+++ app/inuobus/lib/page/qr_scan_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QRScan extends StatefulWidget {
+  QRScan();
+  @override
+  _QRScan createState() => _QRScan();
+}
+
+class _QRScan extends State<QRScan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('QRScan')),
+    );
+  }
+}
