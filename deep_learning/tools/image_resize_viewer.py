e829fa6cc40fec1932e2ad61c48439c1a71c994c
minoring <minho1350@gmail.com>
2021-03-16 19:28:44 +0900

    impl: image_resize_viewer 구현. 사이즈 줄인 이미지 보고싶을때 사용.

---
+++ deep_learning/tools/image_resize_viewer.py
@@ -0,0 +1,53 @@
+"""이미지 디렉토리, 원하는 이미지 사이즈를 입력값으로 주면 resize된 이미지들을 보여주는 툴.
+
+이미지 resize하면 PET 글씨가 너무 작아져서 안보일까 테스트 할때 씀.
+사용 예제:
+python tools/image_size_viewer.py --image_dir="path/to/image/dir" --image_size=224
+"""
+from pathlib import Path
+import itertools
+
+import matplotlib.pyplot as plt
+from PIL import Image
+from absl import app
+from absl import flags
+
+
+def handle_close(_):
+  global closed
+  closed = True
+
+
+fig = plt.figure()
+closed = False
+fig.canvas.mpl_connect("close_event", handle_close)
+
+
+def imshow_wait_input(img):
+  """키보드 입력, 마우스클릭 기다리는 imshow"""
+  plt.imshow(img)
+  plt.draw()
+  plt.waitforbuttonpress(0)
+
+
+def main(_):
+  dir_path = Path(flags.FLAGS.image_dir)
+  if not dir_path.is_dir():
+    print(f"No such directory {dir_path}")
+    exit(1)
+
+  # jpg, jpeg 이미지들만 보여줌. png등 다른포멧은 지원안함.
+  paths = itertools.chain((dir_path.rglob("*.jpeg")), dir_path.rglob("*.jpg"))
+  for path in paths:
+    img = Image.open(path)
+    new_size = (flags.FLAGS.image_size, flags.FLAGS.image_size)
+    imshow_wait_input(img.resize(new_size))
+
+    if closed:
+      break
+
+
+if __name__ == "__main__":
+  flags.DEFINE_string("image_dir", None, "이미지 폴더 경로")
+  flags.DEFINE_integer("image_size", None, "원하는 이미지 사이즈")
+  app.run(main)
