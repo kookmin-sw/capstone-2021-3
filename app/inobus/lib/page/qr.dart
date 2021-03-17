4cb6326f4c8312cdf6ee8992da6f2ffb0c1dc804
TongSeola <waitflowerofeast@gmail.com>
2021-03-18 00:45:57 +0900

    Make QR Code Scanner

--- app/inobus/lib/page/qr.dart
+++ app/inobus/lib/page/qr.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 class QRPage extends StatefulWidget {
   QRPage();
@@ -7,8 +10,47 @@ class QRPage extends StatefulWidget {
 }
 
 class _QRPage extends State<QRPage> {
+  String result = "press the camera to start the scan !";
+
+  Future scanQR() async {
+    try {
+      ScanResult qrScanResult = await BarcodeScanner.scan();
+      String qrResult = qrScanResult.rawContent;
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.cameraAccessDenied) {
+        setState(() {
+          result = "Camera was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('QR'));
+    return Scaffold(
+      body: Center(
+        child: Text(result),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: scanQR,
+      ),
+    );
   }
 }
