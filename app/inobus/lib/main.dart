15a6dfde133ee71e4145d6e5c9bb9111f3793327
TongSeola <waitflowerofeast@gmail.com>
2021-03-26 17:49:23 +0900

    구현 : 앱 소개 페이지 작성 - 이노버스 제공 이미지 - 수직으로 잘라서 구현 - smooth_page_indicator 사용
    issue : #44

--- app/inobus/lib/main.dart
+++ app/inobus/lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
 
 import 'frame/home.dart';
+import 'frame/introduce.dart';
 
 void main() {
-  runApp(InuobusApp());
+  runApp(InobusApp());
 }
 
-class InuobusApp extends StatelessWidget {
+class InobusApp extends StatefulWidget {
+  @override
+  _InobusApp createState() => _InobusApp();
+}
+
+class _InobusApp extends State<InobusApp> {
+  bool checking = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'INOBUS application',
-      home: HomeFrame(
-        title: 'INOBUS',
-        backgroundColor: Colors.green,
-        pointColor: Colors.white,
-      ),
+      home: checking
+          ? HomeFrame(
+              title: 'INOBUS',
+              backgroundColor: Colors.orange,
+              pointColor: Colors.white,
+            )
+          : IntroduceFrame(
+              onPressButton: () {
+                setState(() {
+                  checking = true;
+                });
+              },
+            ),
     );
   }
 }
