11c1c314e3170241234eae3f7f5fa137b5fee302
TongSeola <waitflowerofeast@gmail.com>
2021-03-28 01:39:13 +0900

    구현 : PNG 파일을 이요한 소개 페이지 배경 작성 - 동그라미 위치가 지금은 절대값
    issue #80

--- app/inobus/lib/frame/introduce.dart
+++ app/inobus/lib/frame/introduce.dart
@@ -3,8 +3,10 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class IntroduceFrame extends StatefulWidget {
   final VoidCallback onPressButton;
+  final Color backgroundColor;
 
-  IntroduceFrame({Key key, this.onPressButton}) : super(key: key);
+  IntroduceFrame({Key key, this.onPressButton, this.backgroundColor})
+      : super(key: key);
 
   @override
   _IntroduceFrame createState() => _IntroduceFrame();
@@ -13,16 +15,30 @@ class IntroduceFrame extends StatefulWidget {
 class _IntroduceFrame extends State<IntroduceFrame> {
   int selectedIndex;
   PageController pagecontroller;
-  List<Image> imgList = [];
+  List<Container> imgList = [];
   List<Widget> pageList = [];
 
   void getIntroduceImg() {
-    imgList.add(Image.asset('assets/introduce/introduce1.jpg'));
-    imgList.add(Image.asset('assets/introduce/introduce2_1.jpg'));
-    imgList.add(Image.asset('assets/introduce/introduce2_2.jpg'));
-    imgList.add(Image.asset('assets/introduce/introduce2_3.jpg'));
-    imgList.add(Image.asset('assets/introduce/introduce2_4.jpg'));
-    imgList.add(Image.asset('assets/introduce/introduce2_5.jpg'));
+    imgList.add(Container(
+        child: Image.asset(
+      'assets/introduce/introduce1.jpg',
+      fit: BoxFit.fitWidth,
+    )));
+    imgList.add(Container(
+        child: Image.asset('assets/introduce/introduce.PNG',
+            fit: BoxFit.fitWidth)));
+    imgList.add(Container(
+        child: IntroducePage(
+            namePNG: "introduce1",
+            text: "TEST1",
+            number: 1,
+            baseColor: widget.backgroundColor)));
+    imgList.add(Container(
+        child: IntroducePage(
+            namePNG: "introduce2",
+            text: "TEST2",
+            number: 2,
+            baseColor: widget.backgroundColor)));
   }
 
   // 손으로 페이지를 넘겼을 때
@@ -40,8 +56,8 @@ class _IntroduceFrame extends State<IntroduceFrame> {
 
   @override
   void initState() {
-    getIntroduceImg();
     super.initState();
+    getIntroduceImg();
     this.selectedIndex = 0;
     this.pagecontroller = PageController(
       initialPage: this.selectedIndex,
@@ -64,16 +80,68 @@ class _IntroduceFrame extends State<IntroduceFrame> {
           ),
           Positioned(
               top: mediaQuery.size.height * 0.3,
+              left: mediaQuery.size.width * 0.02,
               child: SmoothPageIndicator(
                   controller: pagecontroller, // PageController
                   count: imgList.length,
                   axisDirection: Axis.vertical,
-                  effect: WormEffect(), // your preferred effect
+                  effect: WormEffect(
+                      // paintStyle: PaintingStyle.stroke,
+                      dotColor: Colors.grey.withOpacity(.60),
+                      activeDotColor: Colors.blue), // your preferred effect
                   onDotClicked: (index) {}))
         ],
       ),
       floatingActionButton:
           TextButton(child: Text("소개 페이지 닫기"), onPressed: widget.onPressButton),
     );
+  }
+}
+
+// IntroducePage(
+//   namePNG: "introduce1",
+//   text: "test1",
+//   number: 1,
+//   baseColor: widget.backgroundColor));
+class IntroducePage extends StatelessWidget {
+  final String namePNG;
+  final String text;
+  final int number;
+  final Color baseColor;
+
+  IntroducePage({Key key, this.namePNG, this.text, this.number, this.baseColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/introduce/${this.namePNG}.PNG"),
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        child: Stack(children: [
+          Positioned(top: 400, left: 200, child: Text(this.text)),
+          Positioned(
+              top: 400,
+              left: 100,
+              child: Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(
+                    width: 8,
+                    color: this.baseColor,
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                child: Center(
+                    child: Text(this.number.toString(),
+                        style:
+                            TextStyle(color: this.baseColor, fontSize: 50.0))),
+              )),
+        ]));
   }
 }
