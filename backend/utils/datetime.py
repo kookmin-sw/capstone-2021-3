6ffe01c16204ec0b70f4cd49058a3612ff34f28d
Aqudi <gjdigj145@gmail.com>
2021-04-02 02:38:15 +0900

    구현: str to datetime 유틸, logger 유틸 구현

--- backend/utils/datetime.py
+++ backend/utils/datetime.py
@@ -2,4 +2,8 @@ from datetime import datetime
 
 
 def get_current_datetime_str() -> str:
-    return datetime.now().strftime("%Y-%M-%d %I:%M:%S %p")
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S %p")
+
+
+def get_datetime_obj_from_str(date: str) -> datetime:
+    return datetime.strptime(date, "%Y-%m-%d %H:%M:%S %p")
