21be0224726aa36e517acd29780052910589650f
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2021-04-01 20:17:56 +0900

    구현: 모든 쓰샘 기기의 리스트 조회 #62

--- backend/routes/devices.py
+++ backend/routes/devices.py
@@ -2,6 +2,7 @@ from typing import List
 
 from fastapi import APIRouter
 
+from database import db
 from models.device import Device
 
 router = APIRouter()
@@ -13,8 +14,7 @@ router = APIRouter()
     description="모든 쓰샘 기기의 리스트 조회",
 )
 async def deivce_list():
-    pass
-
+    return [Device(**i) for i in db.devices.find()]
 
 @router.get(
     "/nearby",
