84d2a265eceae31f25cc2c6ce28c9863ebab7cc4
TongSeola <waitflowerofeast@gmail.com>
2021-04-04 02:15:42 +0900

    리팩터 : 변경된 배경,이미지 변경 - 새로 변경된 디자인 적용 issue #80

--- app/inobus/lib/main.dart
+++ app/inobus/lib/main.dart
@@ -13,10 +13,27 @@ class InobusApp extends StatefulWidget {
 }
 
 class _InobusApp extends State<InobusApp> {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPage createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
   bool checking = false;
-  Color representativeColor = Color(0xffF04C18);
+  final Color representativeColor = Color(0xffF04C18);
+
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var screenWidth = screenSize.width;
+    var screenHeight = screenSize.height;
     return MaterialApp(
         title: 'INOBUS application',
         home: checking
@@ -26,6 +43,8 @@ class _InobusApp extends State<InobusApp> {
                 pointColor: Colors.white,
               )
             : IntroduceFrame(
+                screenHeight: screenHeight,
+                screenWidth: screenWidth,
                 backgroundColor: representativeColor,
                 onPressButton: () {
                   setState(() {
@@ -39,3 +58,41 @@ class _InobusApp extends State<InobusApp> {
         ));
   }
 }
+
+
+// class _InobusApp extends State<InobusApp> {
+//   bool checking = false;
+//   final Color representativeColor = Color(0xffF04C18);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var screenSize = MediaQuery.of(context).size;
+//     var width = screenSize.width;
+//     var height = screenSize.height;
+//     print("높이 : " + width.toString());
+//     print("넓이 : " + height.toString());
+//     print("프린트문 확인");
+//     return MaterialApp(
+//         title: 'INOBUS application',
+//         home: checking
+//             ? HomeFrame(
+//                 title: 'INOBUS',
+//                 backgroundColor: representativeColor,
+//                 pointColor: Colors.white,
+//               )
+//             : IntroduceFrame(
+//                 // screenHeight: ,
+//                 // screenWidth: ,
+//                 backgroundColor: representativeColor,
+//                 onPressButton: () {
+//                   setState(() {
+//                     checking = true;
+//                   });
+//                 },
+//               ),
+//         theme: ThemeData(
+//           // 테마 색상 설정
+//           accentColor: representativeColor,
+//         ));
+//   }
+// }
