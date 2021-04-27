a88a89bc8613b99ea997131e8f12dfd2da0e44cf
TongSeola <waitflowerofeast@gmail.com>
2021-04-27 20:34:45 +0900

    구현 : 메뉴 구성 - 메뉴에 구성할 부분 틀 만들기 #98

---
+++ app/inobus/lib/page/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage();
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("로그인"),
+        ),
+        body: Text('LogIn'));
+  }
+}
