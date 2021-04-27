fd33aeb725abe2b9e4156786c8fbcb9b9f4b66d8
TongSeola <waitflowerofeast@gmail.com>
2021-04-28 00:37:42 +0900

    구현 : 구글 로그인 안드로이드 - 구글 로그인 안드로이드 버전 완성 issue #100

--- app/inobus/lib/page/login.dart
+++ app/inobus/lib/page/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage();
@@ -7,13 +9,60 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPage extends State<LoginPage> {
+  bool _isLoggedIn = false;
+  GoogleSignInAccount _userObj;
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+  login() async {
+    try {
+      await _googleSignIn.signIn();
+      setState(() {
+        _isLoggedIn = true;
+        _userObj = _googleSignIn.currentUser;
+      });
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  logout() {
+    _googleSignIn.signOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("로그인"),
+      appBar: AppBar(title: Text("Log In")),
+      body: Container(
+        child: _isLoggedIn
+            ? Center(
+                child: Column(
+                children: <Widget>[
+                  Image.network(_userObj.photoUrl),
+                  Text(_userObj.displayName),
+                  Text(_userObj.email),
+                  ElevatedButton(
+                      onPressed: () {
+                        logout();
+                      },
+                      child: Text("Logout"))
+                ],
+              ))
+            : Center(
+                child: OutlinedButton.icon(
+                label: Text("Login with Google"),
+                onPressed: () {
+                  login();
+                },
+                icon: FaIcon(
+                  FontAwesomeIcons.google,
+                  color: Colors.red,
+                ),
+              )),
       ),
-      body: Text('Log in'),
     );
   }
 }
