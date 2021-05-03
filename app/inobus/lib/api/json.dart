fa70f6c4d2c056c47ab882be7f9a27159a1efbf2
TongSeola <waitflowerofeast@gmail.com>
2021-05-04 01:46:41 +0900

    구현 : API orgainzation 연결 - 한글깨짐 문제 해결 Issue #107

--- app/inobus/lib/api/json.dart
+++ app/inobus/lib/api/json.dart
@@ -1,16 +1,35 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 
 List data;
 List<String> result = [];
 
 Future<List<String>> requestOrganization() async {
-  var jsonText = await rootBundle.loadString('assets/test/organizations.json');
-  data = json.decode(jsonText);
   result = [];
-  for (int i = 0; i < data.length; i++) {
-    result.add(data[i]['name']);
+  String url =
+      "http://ec2-54-149-103-226.us-west-2.compute.amazonaws.com/api/v1/organizations/";
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var responseBody = utf8.decode(response.bodyBytes); //String
+    var data = json.decode(responseBody); //json
+    print(data);
+
+    for (int i = 0; i < data.length; i++) {
+      result.add(data[i]["name"]);
+    }
+  } else {
+    print("Can not access API");
+    print(response.statusCode);
   }
+  // 이후 생략되야 하는 코드
+  // var jsonText = await rootBundle.loadString('assets/test/organizations.json');
+  // print(jsonText.runtimeType);
+  // data = json.decode(jsonText);
+  // for (int i = 0; i < data.length; i++) {
+  //   result.add(data[i]['name']);
+  // }
+  //
   return result;
 }
