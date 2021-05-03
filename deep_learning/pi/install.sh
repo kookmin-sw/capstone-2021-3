ec365de429f2dbbd84488e55115c86bcafc380b9
minoring <minho1350@gmail.com>
2021-05-03 23:07:17 +0900

    구현: 라즈베리파이 tflite mobilenet inference 구현

---
+++ deep_learning/pi/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TFLite runtime
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python3-tflite-runtime
+
+# OpenCV
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+sudo apt-get -y install libhdf5-dev libqtgui4  libqt4-test
+pip3 install opencv-python
