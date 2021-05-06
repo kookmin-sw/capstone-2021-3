a9213458b488b821fc06155e2e7dc7bb36a89ce7
TongSeola <waitflowerofeast@gmail.com>
2021-05-06 23:12:45 +0900

    구현: 새로운 디자인 적용 - makepage에 몰아 넣어서 만듦 Isseu #115

--- app/inobus/lib/main.dart
+++ app/inobus/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'frame/home.dart';
-import 'frame/introduce.dart';
+import 'makepage/loginPage.dart';
+// import 'frame/home.dart';
+// import 'frame/introduce.dart';
 
 void main() {
   runApp(InobusApp());
@@ -27,34 +27,50 @@ class MainPage extends StatefulWidget {
 
 class _MainPage extends State<MainPage> {
   bool checking = false;
-  final Color representativeColor = Color(0xffE8551A);
+  final Color representativeColor = Color(0xff5234eb);
 
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-    var screenWidth = screenSize.width;
-    var screenHeight = screenSize.height;
     return MaterialApp(
         title: 'INOBUS application',
-        home: checking
-            ? HomeFrame(
-                title: 'INOBUS',
-                backgroundColor: representativeColor,
-                pointColor: Colors.white,
-              )
-            : IntroduceFrame(
-                screenHeight: screenHeight,
-                screenWidth: screenWidth,
-                backgroundColor: representativeColor,
-                onPressButton: () {
-                  setState(() {
-                    checking = true;
-                  });
-                },
-              ),
+        home: LoginSNSPage(),
         theme: ThemeData(
           // 테마 색상 설정
           accentColor: representativeColor,
         ));
   }
 }
+
+// class _MainPage extends State<MainPage> {
+//   bool checking = false;
+//   final Color representativeColor = Color(0xffE8551A);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var screenSize = MediaQuery.of(context).size;
+//     var screenWidth = screenSize.width;
+//     var screenHeight = screenSize.height;
+//     return MaterialApp(
+//         title: 'INOBUS application',
+//         home: checking
+//             ? HomeFrame(
+//                 title: 'INOBUS',
+//                 backgroundColor: representativeColor,
+//                 pointColor: Colors.white,
+//               )
+//             : IntroduceFrame(
+//                 screenHeight: screenHeight,
+//                 screenWidth: screenWidth,
+//                 backgroundColor: representativeColor,
+//                 onPressButton: () {
+//                   setState(() {
+//                     checking = true;
+//                   });
+//                 },
+//               ),
+//         theme: ThemeData(
+//           // 테마 색상 설정
+//           accentColor: representativeColor,
+//         ));
+//   }
+// }
