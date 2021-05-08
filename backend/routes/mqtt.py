9615fe7730b48d8fe1595f8d8b6b762c892e63e0
Aqudi <gjdigj145@gmail.com>
2021-05-08 14:57:44 +0900

    구현: MQTT 요청 로그 수정

--- backend/routes/mqtt.py
+++ backend/routes/mqtt.py
@@ -39,7 +39,7 @@ def connect(client, flags, rc, properties):
     TOPIC_CAPACITY : 모든 기관의 PLASTIC, WATER 용량
     TOPIC_POINT    : 모든 기관의 POINT 기록
     """
-    logger.debug(f"Connected: {client}, {flags}, {rc}, {properties}")
+    logger.info(f"MQTT Broker에 연결되었습니다.")
 
     # 적재량 Topic 구독
     mqtt.client.subscribe(TOPIC_CAPACITY)
@@ -50,13 +50,13 @@ def connect(client, flags, rc, properties):
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
     """MQTT Broker와 연결이 끊어졌을 때 동작하는 Handler"""
-    logger.debug(f"Disconnected client '{client._client_id}'")
+    logger.info(f"MQTT Broker와 연결이 끊어졌습니다.")
 
 
 @mqtt.on_subscribe()
 def subscribe(client, mid, qos, properties):
     """특정 Topic을 구독했을 때 동작하는 Handler"""
-    logger.debug(f"Subscribed client '{client._client_id}', {mid}, {qos}, {properties}")
+    logger.info(f"토픽을 구독합니다.: 'client_id: {client._client_id}'")
 
 
 @mqtt.subscribe(TOPIC_CAPACITY, TOPIC_POINT)
@@ -64,7 +64,7 @@ async def handle_data_topics(client, topic, payload, qos, properties):
     """TOPIC_CAPACITY, TOPIC_POINT Handler"""
     try:
         payload = payload.decode()
-        logger.debug(f"Topic: {topic}, Msg: {payload}, From: {client._client_id}")
+        logger.info(f"데이터 수신, 토픽: {topic}, Msg: {payload}, From: {client._client_id}")
 
         # Topic에서 정보를 추출
         topic_list = topic.split("/")
@@ -73,7 +73,6 @@ async def handle_data_topics(client, topic, payload, qos, properties):
 
         # Topic에서 추출한 정보와 시간 정보를 json에 업데이트
         json_data = json.loads(payload)
-        json_data["date"] = parse_datetime_str(json_data["date"])
         json_data["organization_name"] = organization_name
         json_data["device_name"] = device_name
 
@@ -81,29 +80,33 @@ async def handle_data_topics(client, topic, payload, qos, properties):
             # 데이터 validation
             CapacityData.validate(json_data)
             # Capacity data 삽입
-            db.capacities.insert_one(json_data)
+            result = db.capacities.insert_one(json_data)
         elif topic_list[1] in TOPIC_POINT:
             # 데이터 validation
             PointData.validate(json_data)
             # Point data 삽입
-            db.points.insert_one(json_data)
+            result = db.points.insert_one(json_data)
+
             # Organization의 Point + 1
-            db.organizations.update(
+            result = db.organizations.update_one(
                 {"name": organization_name},
                 {"$inc": {"point": 1}},
                 upsert=True,  # 찾는 document가 없을 시 생성
             )
+
             # User의 Point + 1
             user = json_data.get("user")
             if user:
-                db.users.update(
+                result = db.users.update_one(
                     {"name": user},
                     {"$inc": {"point": 1}},
                     upsert=True,  # 찾는 document가 없을 시 생성
                 )
     except JSONDecodeError as e:
-        logger.error(f"{topic}, {payload}, {e}")
+        logger.error(f"올바르지 않은 포맷, 토픽: {topic}, Msg: {payload}, Error: {e}")
     except IndexError as e:
-        logger.error(f"{topic}, {payload}, {e}")
+        logger.error(f"인덱스 에러, 토픽: {topic}, Msg: {payload}, Error: {e}")
     except Exception as e:
-        logger.error(f"{topic}, {payload}, {e}")
+        logger.error(f"알 수 없는 에러, 토픽: {topic}, Msg: {payload}, Error: {e}")
+    else:
+        logger.info(f"정상 처리, 토픽: {topic}, Msg: {payload}")
