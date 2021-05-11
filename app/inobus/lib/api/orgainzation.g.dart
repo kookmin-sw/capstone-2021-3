c24cba6b580d109815a84f56ff12b03d999aa008
TongSeola <waitflowerofeast@gmail.com>
2021-05-11 17:06:09 +0900

    구현 : JsonSerializable 적용 - 기관이름, 포인트 class에 JsonSerializable 적용

---
+++ app/inobus/lib/api/orgainzation.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'orgainzation.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Orgainzation _$OrgainzationFromJson(Map<String, dynamic> json) {
+  return Orgainzation(
+    json['name'] as String,
+    json['point'] as int,
+  );
+}
+
+Map<String, dynamic> _$OrgainzationToJson(Orgainzation instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'point': instance.point,
+    };
