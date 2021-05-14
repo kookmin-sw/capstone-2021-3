e8ede49aa2ffd5998fd34701daac0893973b43ad
TongSeola <waitflowerofeast@gmail.com>
2021-05-14 17:11:51 +0900

    추가 : 비밀키 올리기

--- app/inobus/ios/Runner/AppDelegate.swift
+++ app/inobus/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("${env.GOOGLE_MAP_API_KEY}") //for google_maps_flutter.dart.dart
+    GMSServices.provideAPIKey("AIzaSyCbBmX-gTCMbHIKQJtA55o2DFUn4jhT9w4") //for google_maps_flutter.dart.dart
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
