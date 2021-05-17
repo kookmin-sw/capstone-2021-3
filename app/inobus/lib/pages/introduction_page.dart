8fbfd7b248c205b91a65d102d6514d3557882cff
TongSeola <waitflowerofeast@gmail.com>
2021-05-17 23:57:32 +0900

    수정 : 필요 없는 페이지 삭제 - 바코드 닉네임 만들어 주는 페이지 삭제

--- app/inobus/lib/pages/introduction_page.dart
+++ app/inobus/lib/pages/introduction_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inobus/pages/introduction/1_login_page.dart';
 import 'package:inobus/pages/introduction/2_after_login_page.dart';
-import 'package:inobus/pages/introduction/3_make_barcode_page.dart';
 import 'package:inobus/routes.dart';
 
 class IntroductionPage extends StatelessWidget {
@@ -24,8 +23,7 @@ class IntroductionPage extends StatelessWidget {
       controller: _pageController,
       children: [
         LoginPage(onNextPage: onNextPage(1)),
-        AfterLoginPage(onNextPage: onNextPage(2)),
-        MakeBarcodePage(
+        AfterLoginPage(
           onNextPage: () => Navigator.pushNamed(context, Routes.map),
         ),
       ],
