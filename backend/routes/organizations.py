a693cfe1cae9223c955606b0f5100a0f76a6270f
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2021-05-22 16:01:14 +0900

    수정: 코드리뷰 수정
    + 기관명->기관코드
    + 테스트코드 삭제

--- backend/routes/organizations.py
+++ backend/routes/organizations.py
@@ -3,8 +3,10 @@ from typing import List
 from fastapi import APIRouter, HTTPException
 
 from database import db
+from models.device import Device
 from models.organization import Organization
 from models.user import UserOut
+from utils.pyobjectid import ObjectId, PyObjectId
 
 router = APIRouter()
 
@@ -19,45 +21,23 @@ async def organization_list():
 
 
 @router.get(
-    "/{organization_name}",
-    response_model=Organization,
+    "/{organization}/devices",
+    response_model=List[Device],
     description="입력한 기관 이름으로 정보 조회",
 )
-async def organization_detail(organization_name: str):
-    res = list(db.organizations.find({"name": organization_name}))
-    if len(res) == 1:
-        return res[0]
-    elif len(res) == 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-    else:
-        raise HTTPException(status_code=404, detail="Too many result")
+async def organization_detail(organization: PyObjectId):
+    res = list(db.devices.find({"organization": organization}))
+    return res
 
 
 @router.get(
-    "/{organization_name}/users",
+    "/{organization}/users",
     response_model=List[UserOut],
     description="기관에 속한 개인의 리스트 조회",
 )
-async def organization_user_list(organization_name: str):
-    print(db.users.find({"organization_name": organization_name}).sort("point", -1))
+async def organization_user_list(organization: PyObjectId):
+    print(db.users.find({"organization": organization}).sort("point", -1))
     return [
         UserOut(**i)
-        for i in db.users.find({"organization_name": organization_name}).sort(
-            "point", -1
-        )
+        for i in db.users.find({"organization": organization}).sort("point", -1)
     ]
-
-
-@router.get(
-    "/{organization_name}/users/{user_name}",
-    response_model=UserOut,
-    description="기관에 속한 특정 개인의 정보 조회",
-)
-async def organization_user_detail(organization_name: str, user_name: str):
-    res = list(
-        db.users.find({"organization_name": organization_name, "user_name": user_name})
-    )
-    if len(res) == 1:
-        return res[0]
-    elif len(res) == 0:
-        raise HTTPException(status_code=404)
