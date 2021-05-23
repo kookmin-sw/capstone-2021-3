c920d2440ba949380a28de0b7bae273f1ebc16e6
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2021-05-23 17:09:38 +0900

    구현: 웹 컵 등록부 MQTT전환 및 프론트 수정

--- iot/routes/home.py
+++ iot/routes/home.py
@@ -1,7 +1,8 @@
+import json
 from typing import Optional
 
 import requests
-from fastapi import APIRouter, FastAPI, Request, WebSocket
+from fastapi import APIRouter, FastAPI, Request, WebSocket, params
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
@@ -17,16 +18,30 @@ router = APIRouter()
 
 masterws = [None]
 
+insert_data_id = [None]
+
+base_url = config.api_settings.base_url
+
+org_id = db.find_one(DBType.organization)
+device_id = db.find_one(DBType.device)
+org_id = "60901b909232116ad8c4f0d6"
+device_id = "609a71f5f76202373c88040a"
+
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    title = "Dashboard"
+    point = -1
+    res = requests.get(base_url + "/organizations")
+    res = json.loads(res.text)
+    for i in res:
+        if i["_id"] == org_id:
+            point = i["point"]
 
     return templates.TemplateResponse(
         "home.html",
         {
             "request": request,
-            "title": title,
+            "point": point,
         },
     )
 
@@ -37,7 +52,23 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        op = data.split(":")[0]
+        if op == "reward":
+            print("code: " + data.split(":")[1])
+
+            uid = data.split(":")[1]
+            uid = "Mcw8DUx6mMRZRfJl3o1WzJRgqZK2"
+            # data_id = db.find_one(DBType.last_point)
+            data_id = "60a9f67f6dee8ef00ab0a4a4"
+
+            res = requests.post(
+                base_url + "/rewards/person_reward",
+                params={"device_id": device_id, "uid": uid, "data_id": data_id},
+            )
+            print(res.url)
+            print(res.text)
+        await websocket.send_text(f"Message text was: {res.text}")
+        # await websocket.send_text(f"Message text was: {data}")
 
 
 @router.get("/hello")
@@ -49,6 +80,25 @@ async def home(request: Request):
 
 @router.get("/insert_cup")
 async def home(request: Request):
-    await masterws[0].send_text("inserted")
+    try:
+        await masterws[0].send_text("inserted")
 
-    return "hello"
+        return "hello"
+    except:
+        return "no"
+
+
+# @router.get("/insert_cup")
+# async def home(request: Request):
+#     res = requests.post(base_url+'/rewards/insert_cup', params = {'device_id': device_id})
+#     res = json.loads(res.text)
+#     if res['result'] == "success":
+#         insert_data_id[0] = res['data_id']
+#         try:
+#             await masterws[0].send_text("inserted")
+
+#             return "hello"
+#         except:
+#             return "no"
+#     else:
+#         return "device id error"
